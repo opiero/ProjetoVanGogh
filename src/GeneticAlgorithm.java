aff02d635b830ceaaa92dc4a310da9180b949429
Piero Capelo <piero.capelo@gmail.com>
2017-12-10 12:52:46 -0200

    Created some interface

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -227,4 +227,12 @@ public class GeneticAlgorithm {
     public BufferedImage getTarget() {
         return target;
     }
+
+    public void setTheBestEver(Genome theBestEver) {
+        this.theBestEver = theBestEver;
+    }
+
+    public void setAlGenomes(ArrayList<Genome> alGenomes) {
+        AlGenomes = alGenomes;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,31 +8,50 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String [] args) {
+    public static void main (String [] args) throws Exception {
 
-        /*System.out.printf("Tecle o nome do arquivo da imagem: ");
+        System.out.printf("Tecle o nome do arquivo da imagem: ");
 
         Scanner input = new Scanner(System.in);
-        String imageName = input.nextLine();*/
+        String imageName = input.nextLine();
+
+        System.out.printf("Quantas gerações gostaria de rodar? ");
+
+        int numGenerations = input.nextInt();
+
+        System.out.printf("Há algum fenótipo inicial?(y/n) ");
+
+        String yesOrNo = input.nextLine();
+
+        String starterName = null;
+
+        if (yesOrNo == "y") {
+
+            System.out.printf("Qual? ");
+            starterName = input.nextLine();
+
+        }
 
         BufferedImage image = null;
 
-        try {
-            image = ImageIO.read(new File("n.jpg"));
-        } catch (IOException e) {
-            System.out.println("Erro! Não pude encontrar a imagem");
-        }
+
+        image = ImageIO.read(new File(imageName));
 
         GeneticAlgorithm GA = new GeneticAlgorithm(10, 50, 0.15, 1000,
                 0, image.getWidth(), 0, image.getHeight(), image);
-
-        int numGenerations = 10000;
 
         for (int i = 0; i < numGenerations; i++) {
 
             System.out.println("Geração " + GA.getCurrentGeneration() + " de " + numGenerations);
             GA.runGeneration();
             System.out.println("Fitness do melhor de todos: " + GA.getTheBestEverFitness());
+
+            if (i % 100 == 0){
+                File output = new File (imageName + i + ".jpg");
+
+                ImageIO.write(GA.getTheBestEver().getPhenotype(), "jpg", output);
+
+            }
 
 
         }
